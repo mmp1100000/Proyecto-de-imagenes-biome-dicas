550b7ee3ea4ebde8530d5673bf87ef7e1da5a214
Edusitoo <0619484299@uma.es>
2017-10-23 22:11:12 +0200

    New Function select_image

---
+++ Scripts creados/select_image.m
@@ -0,0 +1,40 @@
+function [Image] = select_image( uigetdir )
+%This function receive a directory and show all the images inside it. Then, 
+%when you press "scape", the function return the image that you were seeing
+
+
+%%Turning off warnings
+%[a,MSGID] = lastwarn()
+warning('off','images:dicominfo:fileVRDoesNotMatchDictionary'); %Turn off Marco's programm warning
+warning('off','MATLAB:nargchk:deprecated'); %Turn off getkey news
+%%
+
+%uigetdir creates a visual interface for selecting the images location
+
+dicomlist = loadDicom(uigetdir);
+cnt = 1;
+key = 0;
+
+while key ~= 27
+    
+    I = dicomread(dicomlist(cnt));
+    imshow(I,[]);
+    
+    key = getkey;
+    
+    if key ~= 27 && key ~= 28 && key ~= 29
+        key = getkey;
+    end
+    
+    if key == 29 && cnt ~= numel(dicomlist)
+        cnt = cnt + 1;
+    else
+        if key == 28 && cnt ~= 1
+            cnt = cnt - 1;
+        end
+    end  
+end %Show images
+
+
+end
+
