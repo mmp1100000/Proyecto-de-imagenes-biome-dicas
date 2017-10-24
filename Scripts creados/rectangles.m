0b6458942c53650e178aedb674378a10565def2c
mmp1100000 <marcomunozperez@uma.es>
2017-10-24 15:13:55 +0200

    Changes in pixel input

--- Scripts creados/rectangles.m
+++ Scripts creados/rectangles.m
@@ -7,14 +7,15 @@ prompt = 'How many rectangles do you want? \n';
 number_of_rectangles = input(prompt);
 
 
-[X,Y] = ginput(2*number_of_rectangles);
+%[X,Y] = ginput(2*number_of_rectangles);
 
 c0=2;
 initialLSF = c0*ones(size(Image));
 % generate the initial region R0 as two rectangles
 for cnt = 1:number_of_rectangles
-    
-initialLSF(Y(2*cnt-1) : Y(2*cnt),X(2*cnt-1) : X(2*cnt))= -c0;
+[X,Y] = ginput(2);    
+%initialLSF(Y(2*cnt-1) : Y(2*cnt),X(2*cnt-1) : X(2*cnt))= -c0;
+initialLSF(Y(1) : Y(2),X(1) : X(2))= -c0;
 phi=initialLSF;
 hold on;
 contour(phi, [0,0], 'r','LineWidth',2);
