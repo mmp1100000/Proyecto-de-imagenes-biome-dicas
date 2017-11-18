43975e90b4701fac2a550ed3a2ef8f7bca8f218e
mmp1100000 <marcomunozperez@uma.es>
2017-11-18 14:51:07 +0100

    Non-significant modifications

--- Scripts creados/Project.m
+++ Scripts creados/Project.m
@@ -1,7 +1,5 @@
-
 [IInit,cntInit,dicomList] = select_image2();
 phi = rectangles(IInit);
 vari = select_var();
 [phiInit,vari,areaInit,dim]=DRLSE(IInit,phi,vari);
 [Z,C,A]=fetchContours(dicomList,vari,phiInit,cntInit,areaInit);
-
